c7c67a382d9b401c4d5709e71c022d17e8d65a76
YYJ <934780820@qq.com>
2019-04-12 20:28:07 +0800

    ng start

--- typescript-study/14.ts
+++ typescript-study/14.ts
@@ -9,10 +9,10 @@
 // console.log(set<string>('我是', '聪明b'))
 
 // 2
-interface ConfigFn<T> {
-  (v1: T, v2: T): T
-}
-var set: ConfigFn<string> = <T>(v1: T, v2: T): T => {
-  return v1
-}
-console.log(set('我是', '聪明b'))
+// interface ConfigFn<T> {
+//   (v1: T, v2: T): T
+// }
+// var set: ConfigFn<string> = <T>(v1: T, v2: T): T => {
+//   return v1
+// }
+// console.log(set('我是', '聪明b'))
